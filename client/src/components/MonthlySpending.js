70a79647739f497d0bad92e04ca46521f80a684a
Maktor <66954908+Maktor@users.noreply.github.com>
2023-04-26 21:03:52 -0400

    Add files via upload

--- client/src/components/MonthlySpending.js
+++ client/src/components/MonthlySpending.js
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/MonthlySpending.css';
 
-const MonthlySpending = () => {};
+const MonthlySpending = () => {
+    const [monthlyBudget, setMonthlyBudget] = useState('');
+    const [displayBudget, setDisplayBudget] = useState('');
 
-export default MonthlySpending;
+    const handleIt = (event) => {
+        setMonthlyBudget(event.target.value);
+      };
+  
+    // A function for the submit button
+    const submitButton = () => {
+        if (monthlyBudget && !isNaN(monthlyBudget)) {
+          setDisplayBudget(monthlyBudget);
+          localStorage.setItem('monthlyBudget', monthlyBudget);
+        } else {
+          alert('Please enter a valid amount');
+        }
+        setMonthlyBudget('');
+      };
+
+    // Use useEffect to locate and collect the entered budget from local storage (localStorage)
+    useEffect(() => {
+        const storedLocallyBudget = localStorage.getItem('monthlyBudget');
+        if (storedLocallyBudget) {
+          setDisplayBudget(storedLocallyBudget);
+        }
+      }, []);
+
+    // Rendering it to the screen
+    return (
+      <div>
+        <h3>Monthly Spending Budget</h3>
+        <input type="text" placeholder="Enter your budget" value={monthlyBudget} onChange={handleIt}/>
+        <button onClick={submitButton}>Enter</button>
+        {
+        displayBudget && (<p>Your monthly budget is <strong>${displayBudget}</strong></p>)
+        }
+      </div>
+    );
+  };
+  
+  export default MonthlySpending;
