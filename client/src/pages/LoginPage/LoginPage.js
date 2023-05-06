4a57013008a5040f9bd881584b86d117ce4e40dc
Mahdiyar Bahreman <66954908+Maktor@users.noreply.github.com>
2023-05-06 14:38:13 -0400

    Add files via upload

--- client/src/pages/LoginPage/LoginPage.js
+++ client/src/pages/LoginPage/LoginPage.js
@@ -24,8 +24,8 @@ const rincome = "2"
     //use try and catch to test
     try {
       // POST request to the /api/login route using PORT 3000
-      const check = await fetch("http://localhost:3000/api/login/", { method: "POST", mode: "cors", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ username, password}),});
-      //const check = await fetch("https://milestone-2-ncstate-server-1ib376eji-maktor.vercel.app/api/login", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ username, password}),});
+      //const check = await fetch("http://localhost:3000/api/login/", { method: "POST", mode: "cors", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ username, password}),});
+      const check = await fetch("https://milestone-2-ncstate-server-1ib376eji-maktor.vercel.app/api/login", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ username, password}),});
 
       const data = await check.json();
       console.log(data)
@@ -46,8 +46,8 @@ const rincome = "2"
 
     try {
       // POST request to the /api/register route using PORT 3000
-      //const check = await fetch("https://milestone-2-ncstate-server-1ib376eji-maktor.vercel.app/api/register", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({username: registerUsername, password: registerPassword,}),});
-      const check = await fetch("http://localhost:3000/api/register", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({username: registerUsername, password: registerPassword,}),});
+      const check = await fetch("https://milestone-2-ncstate-server-1ib376eji-maktor.vercel.app/api/register", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({username: registerUsername, password: registerPassword,}),});
+      //const check = await fetch("http://localhost:3000/api/register", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({username: registerUsername, password: registerPassword,}),});
 
 
       console.log(username, password, rexpenses, rincome)
