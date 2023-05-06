986c757f2f2e307311bae56a74a1dfc4f19df70a
Carlos <coamox443@gmail.com>
2023-05-05 23:58:45 -0400

    Fixed Pages, Added Calculate Daily Income

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import DashboardPage from "./pages/Dashboard/DashboardPage.js";
 import BudgetInputPage from "./pages/BudgetInput/BudgetInput.js";
+import NecessarySpending from "./pages/NecessarySpending/NecessarySpending";
+import SpendingSummary from "./pages/SpendingSummary/SpendingSummary";
 
 //Using JSX. Controlling the routes
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/budgetinput" element={<BudgetInputPage />} />
+        <Route path="/necessaryspending" element={<NecessarySpending />} />
+        <Route path="/spendingsummary" element={<SpendingSummary />} />
       </Routes>
     </Router>
   );
--- client/src/pages/BudgetInput/BudgetInput.js
+++ client/src/pages/BudgetInput/BudgetInput.js
@@ -5,53 +5,32 @@ function BudgetInput() {
 
 // const [days, setDays] = useState("0");
 const [budget, setBudget] = useState("0");
-const [expenses, setExpenses] = useState("0");
-const [spendings, setSpendings] = useState([]);
 
 const navigate = useNavigate();
-let total = 0
 
 const submitButton = async (event) => {
     event.preventDefault();
     let monthChosen = document.getElementById("month")
-    let days = monthChosen.value
-   if (budget > 0 && expenses >= 0)
+    let month = monthChosen.value
+   if (budget <= 0 )
    {
-    total = (budget - expenses)/days
-    document.getElementById("total").innerHTML = "Daily allowance: " + total;
-   } 
+      alert("Invalid values entered, please try again.")
+   }
    else
    {
-    alert("Invalid values entered, please try again.")
+    localStorage.setItem("month", month);
+    localStorage.setItem("budget", budget);
+    navigate("/necessaryspending")
    }
 
-    console.log(days)
-    console.log(budget)
-    console.log(total)
-  };
 
-  const addNecessarySpending = (event) => {
-    event.preventDefault();
-    const spendingName = document.getElementById("spendingName").value;
-    const spendingAmount = document.getElementById("spendingAmount").value;
-  
-    if (!spendingName) {
-      alert("Please enter a spending name.");
-    } else if (isNaN(spendingAmount)) {
-      alert("Please enter a spending amount.");
-    } else if (spendingAmount < 0) {
-      alert("Spending amount must be greater than or equal to 0.");
-    } else {
-      setSpendings([...spendings, { name: spendingName, amount: spendingAmount }]);
-      document.getElementById("spendingName").value = "";
-      document.getElementById("spendingAmount").value = "";
-    }
+    console.log(budget)
+    console.log(month)
   };
 
   return (
-    <div className="DashboardPage">
-      <h1>Dashboard</h1><br/>
-      <h2>Budget Testing</h2>
+    <div className="BudgetInput">
+      <h1>Budget Input</h1>
       <form onSubmit={submitButton}>
       Month: <select name="month" id="month">
       <option value="31">January</option>
@@ -67,30 +46,11 @@ const submitButton = async (event) => {
       <option value="30">November</option>
       <option value="31">December</option>
       </select><br/><br/>
-        {/* Number of Days: <input type="string" placeholder="Days" value={days} onChange={(e) => setDays(e.target.value)}></input><br/><br/> */}
-        Monthly Expenses: <input type="string" placeholder="Expenses" value={expenses} onChange={(e) => setExpenses(e.target.value)}></input><br/><br/>
-        Monthly Income: <input type="string" placeholder="Budget" value={budget} onChange={(e) => setBudget(e.target.value)}></input><br/><br/>
-        <button type="submit">Calculate</button><br/><br/>
-      </form>
-      <h2 id ="total">N/A</h2><br/>
-
-      <form onSubmit={addNecessarySpending}>
-        <h2>Necessary Spendings</h2> 
-        <label htmlFor="spendingName">Spending Name: </label>
-        <input type="text" id="spendingName" placeholder="Spending Name"/><br/><br/>
-        <label htmlFor="spendingAmount">Spending Amount: </label>
-        <input type="number" id="spendingAmount" placeholder="Spending Amount"/><br/><br/>
-        <button type="submit">Add Spending</button>
+        Monthly Income: <input type="number" placeholder="Budget" value={budget} onChange={(e) => setBudget(e.target.value)}></input><br/><br/>
+        <br/><button type="submit" >Necessary Spending</button><br/><br/>
       </form>
 
-      <div>
-        <ul>
-          {spendings.map((spending, index) => (
-          <li key={index} > {spending.name}: {spending.amount}</li>
-          ))}
-        </ul>
-      </div>
-      <br/><button onClick={function(){navigate("/dashboard");}}>Back</button>
+      <br/><button onClick={function(){navigate("/dashboard");}}>Back</button><br/><br/>
 
     </div>
   );
